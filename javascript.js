7134ae3151174d917aa10d2553e19ee132ab89a3
Tim Yih <timmyyih@gmail.com>
2022-09-10 18:07:43 -0400

    Create playRound() function to simulate a round of rps

--- javascript.js
+++ javascript.js
@@ -3,4 +3,33 @@ function getComputerChoice() {
     return choices[Math.floor(Math.random() * 3)];
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+    let playerLower = playerSelection.toLowerCase();
+    let computerLower = computerSelection.toLowerCase();
+
+    if (playerLower === "rock") {
+        if (computerLower === "rock") {
+            return "Tie! You both chose Rock!";
+        } else if (computerLower === "paper") {
+            return "You lose! Paper beats Rock!";
+        } else {
+            return "You win! Rock beats Scissors!";
+        }
+    } else if (playerLower === "paper") {
+        if (computerLower === "rock") {
+            return "You win! Paper beats Rock!";
+        } else if (computerLower === "paper") {
+            return "Tie! Your both chose Paper!";
+        } else {
+            return "You lose! Scissors beats Paper!";
+        }
+    } else {
+        if (computerLower === "rock") {
+            return "You lose! Rock beats Scissors!";
+        } else if (computerLower === "paper") {
+            return "You win! Scissors beats Paper!";
+        } else {
+            return "Tie! You both chose Scissors!";
+        }
+    }
+}
